7c5265e356b8f2cf3482892b49b85511cf0f4721
tahrodriguess <tahrodriguesd@hotmail.com>
2021-05-15 21:26:25 +0100

    last updates
    New changes now that I can commit through VS

---
+++ script/script.js
@@ -0,0 +1,40 @@
+////////////////USER GENERATOR///////////////////
+document.getElementById('userBtn').addEventListener('click', getData);
+
+function getData(){
+    
+    fetch('https://randomuser.me/api/?results=100')
+        .then(res => res.json())
+        .then(data => {
+             
+             let user =  data.results;
+             
+
+             //get data value
+             let output = "<h2><center>Costumers</center></h2>"
+
+             //ger data. Itwill show only 5 costumers//
+             user.slice(-5).forEach(function(lists) {
+                output += `
+                <div class="user">
+                    <div class="card mt-4 bg-light">
+                        <ul class="list-group">
+                            <li class="list-group-item"><h2>Name: ${lists.name.first}</h2></li>
+                            <li class="list-group-item"><img src="${lists.picture.large}"></li>
+                            <li class="list-group-item">Phone Number: ${lists.cell}</li>
+                            <li class="list-group-item">DOB: ${lists.dob.date}</li>
+                            <li class="list-group-item">Age: ${lists.dob.age}</li>
+                            <li class="list-group-item">Email ID: ${lists.email}</li>
+                            <li class="list-group-item">Gender: ${lists.gender}</li>
+                            <li class="list-group-item">City: ${lists.location.city}</li>
+                            <li class="list-group-item">Country: ${lists.location.country}</li>
+                            <li class="list-group-item">PostCode: ${lists.location.postcode}</li>
+                        </ul>
+                    </div>
+                </div> `;
+            });
+
+            document.getElementById('output').innerHTML= output;
+        });
+    }; 
+
